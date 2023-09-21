f13c82d79bc4d9aff107b674b59d00c7e430cbe2
kashifchopra <choprak@ad.lafayette.edu>
2023-09-21 16:06:03 -0400

    Lab02 9/21/23

--- Lab02/hdl/sevenseg_ext_n.sv
+++ Lab02/hdl/sevenseg_ext_n.sv
@@ -16,16 +16,18 @@ module seven_seg_n(
 		 );
 
    assign dp_n = d[6] || !d[5];
-
+   
    always_comb
    
    if (d[6]) segs_n = 7'b1111111;
    else if (d[4]) segs_n =7'b0111111;
+      
+
    
    else 
      case (d[3:0])     //  gfedcba
-       4'd0: segs_n = 7'b1000000;
-       4'd1: segs_n = 7'b1111001;
+       4'd0: segs_n = 7'b1000000; //0
+       4'd1: segs_n = 7'b1111001; //1
        4'd2: segs_n = 7'b0100100;
        4'd3: segs_n = 7'b0110000;
        4'd4: segs_n = 7'b0011001;
@@ -34,14 +36,13 @@ module seven_seg_n(
        4'd7: segs_n = 7'b1111000;
        4'd8: segs_n = 7'b0000000;
        4'd9: segs_n = 7'b0010000;
-       4'd10: segs_n = 7'b0001000;
-       4'd11: segs_n = 7'b0001100;
+       4'd10: segs_n = 7'b0001000; //a
+       4'd11: segs_n = 7'b0001100; //p
        4'd12: segs_n = 7'b1000110; 
        4'd15: segs_n = 7'b0001110;
+       
        default: segs_n = 7'b1111111;
      endcase
-     
 
-   
 
 endmodule: seven_seg_n
