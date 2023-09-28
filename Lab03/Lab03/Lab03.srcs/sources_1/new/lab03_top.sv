7ea041ee3c68e266fef5a3a19962e27e64124827
kashifchopra <choprak@ad.lafayette.edu>
2023-09-28 17:07:47 -0400

    Lab3 initial commit 2

--- Lab03/Lab03/Lab03.srcs/sources_1/new/lab03_top.sv
+++ Lab03/Lab03/Lab03.srcs/sources_1/new/lab03_top.sv
@@ -21,6 +21,32 @@
 
 
 module lab03_top(
+     input logic clk, rst,
+     output logic [7:0] an_n,
+     output logic [6:0] segs_n,
+     output logic dp_n,
+     inout tmp_scl, // use inout only - no logic
+     inout tmp_sda // use inout only - no logic
+ );
+ logic tmp_rdy, tmp_err; // unused temp controller outputs
+ // 13-bit two's complement result with 4-bit fractional part
+ logic [12:0] temp;
 
-    );
+
+ // instantiate the VHDL temperature sensor controller
+ TempSensorCtl U_TSCTL (
+     .TMP_SCL(tmp_scl), .TMP_SDA(tmp_sda), .TEMP_O(temp),
+     .RDY_O(tmp_rdy), .ERR_O(tmp_err), .CLK_I(clk),
+     .SRST_I(rst)
+ );
+ 
+ 
+ 
+ 
+ 
+ // add additional signals & instantiations here
+ 
+ 
+ 
+ 
 endmodule
